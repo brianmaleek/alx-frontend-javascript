88edfa24a55b7b2172d2863b4a11510d432b9b8c
Brian Maleek <maleekmalik@gmail.com>
2025-09-13 22:24:24 +0300

    Refactor printTeacher function to use function declaration instead of arrow function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,7 +15,7 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string  => {
+function printTeacher(firstName: string, lastName: string): string  {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
