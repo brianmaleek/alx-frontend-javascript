5677a38e52cb86a9ac1ae3eda2003500f7363782
Brian Maleek <maleekmalik@gmail.com>
2025-09-13 23:14:56 +0300

    Refactor printTeacher function to use arrow function syntax for consistency

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,7 +15,7 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = function printTeacher(firstName, lastName) {
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
