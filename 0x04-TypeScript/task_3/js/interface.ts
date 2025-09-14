087047fadb44d561065650450d82a6a84796a341
Brian Maleek <maleekmalik@gmail.com>
2025-09-14 19:06:11 +0300

    feat: define and export RowID type and RowElement interface

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,12 @@
+// Interface ts file
+// Define and export the RowID type and RowElement interface
+
+// Type alias for RowID
+export type RowID = number;
+
+// Interface for a row element
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
