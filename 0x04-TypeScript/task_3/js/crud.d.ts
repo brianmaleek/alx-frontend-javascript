08f9966651776aa4651689970d6e05f3315901ed
Brian Maleek <maleekmalik@gmail.com>
2025-09-14 19:06:00 +0300

    feat: add CRUD function declarations for managing rows

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="./interface.ts" />
+import { RowID, RowElement } from './interface';
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
